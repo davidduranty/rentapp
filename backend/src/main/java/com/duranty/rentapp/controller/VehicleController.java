0a136ca506b372c86bced3af0a6fd718f776b0ac
David Duranty <dadaduranty@gmail.com>
2025-06-27 12:47:00 +0200

    Refactor VehicleController to use ResponseEntity for all endpoints and enhance updateVehicle method with additional fields

--- backend/src/main/java/com/duranty/rentapp/controller/VehicleController.java
+++ backend/src/main/java/com/duranty/rentapp/controller/VehicleController.java
@@ -3,35 +3,71 @@ package com.duranty.rentapp.controller;
 import com.duranty.rentapp.model.Vehicle;
 import com.duranty.rentapp.repository.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/vehicles")
 
 public class VehicleController {
-    @Autowired
-    private VehicleRepository vehicleRepository;
 
-    @GetMapping("/all")
-    public List<Vehicle> getAllVehicles() {
-        return vehicleRepository.findAll();
+    final VehicleRepository vehicleRepository;
+
+    public VehicleController(VehicleRepository vehicleRepository) {
+        this.vehicleRepository = vehicleRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Vehicle>> getAllVehicles() {
+        return new ResponseEntity<>(vehicleRepository.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/search")
-    public List<Vehicle> searchByBrand(@RequestParam String brand) {
-        return vehicleRepository.findByBrand(brand);
+    public ResponseEntity<List<Vehicle>> searchByBrand(@RequestParam String brand) {
+        return new ResponseEntity<>(vehicleRepository.findByBrand(brand), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Vehicle> getVehicleById(@PathVariable Long id) {
+        Optional<Vehicle> vehicule = vehicleRepository.findById(id);
+        if (vehicule.isPresent()) {
+            return new ResponseEntity<>(vehicule.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping("/add")
-    public Vehicle createVehicle(@RequestBody Vehicle vehicle) {
-        return vehicleRepository.save(vehicle);
+    public ResponseEntity<Vehicle> createVehicle(@RequestBody Vehicle vehicle) {
+        return new ResponseEntity<>(vehicleRepository.save(vehicle), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Vehicle> updateVehicle(@PathVariable Long id, @RequestBody Vehicle vehicle) {
+        Optional<Vehicle> vehicule = vehicleRepository.findById(id);
+
+        if (vehicule.isPresent()) {
+            Vehicle existingVehicle = vehicule.get();
+            existingVehicle.setBrand(vehicle.getBrand());
+            existingVehicle.setModel(vehicle.getModel());
+            existingVehicle.setYear(vehicle.getYear());
+            existingVehicle.setDailyPrice(vehicle.getDailyPrice());
+            return new ResponseEntity<>(vehicleRepository.save(existingVehicle), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("/delete/{id}")
-    public void deleteVehicle(@PathVariable Long id) {
-        vehicleRepository.deleteById(id);
+    public ResponseEntity<Void> deleteVehicle(@PathVariable Long id) {
+        Optional<Vehicle> vehicule = vehicleRepository.findById(id);
+        if (vehicule.isPresent()) {
+            vehicleRepository.delete(vehicule.get());
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 }
