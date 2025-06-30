06267f0fcf409c3bd93f0168edfddff9767474c5
David Duranty <dadaduranty@gmail.com>
2025-06-30 12:38:39 +0200

    Suppression des annotations SpringBootApplication et RestController dans UserController

--- backend/src/main/java/com/duranty/rentapp/controller/UserController.java
+++ backend/src/main/java/com/duranty/rentapp/controller/UserController.java
@@ -1,10 +1,5 @@
 package com.duranty.rentapp.controller;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-
-@SpringBootApplication
-@RestController
 public class UserController {
 
 }
